6bfe261a4b2e0d2c791f59c9f31420790ecc669c
fjouault <fjouault>
2005-09-15 10:06:13 +0000

    added "refining" keyword

--- deprecated/org.atl.eclipse.adt.editor/src/org/atl/eclipse/adt/ui/text/IAtlLexems.java
+++ deprecated/org.atl.eclipse.adt.editor/src/org/atl/eclipse/adt/ui/text/IAtlLexems.java
@@ -10,7 +10,7 @@ public interface IAtlLexems {
 	
 	public final static String[] BRACKETS	= { "(", ")", "{", "}" };
 	public final static String[] CONSTANTS	= { "true", "false" };
-	public final static String[] KEYWORDS	= { "not", "and", "or", "xor", "implies", "module", "create", "from", "uses", "helper", "def", "context", "rule", "using", "derived", "to", "mapsTo", "distinct", "foreach", "in", "do", "if", "then", "else", "endif", "let", "library", "query", "for", "div" };
+	public final static String[] KEYWORDS	= { "not", "and", "or", "xor", "implies", "module", "create", "from", "uses", "helper", "def", "context", "rule", "using", "derived", "to", "mapsTo", "distinct", "foreach", "in", "do", "if", "then", "else", "endif", "let", "library", "query", "for", "div", "refining" };
 	public final static String[] OPERATORS	= { ">", ".", "->", "-", "*", "/", "+", "=", ">", "<", ">=", "<=", "<>", "<-" };
 	public final static String[] SYMBOLS	= { "!", ",", ";", ":", "|" };
 	public final static String[] TYPES		= { "Bag", "Set", "OrderedSet", "Sequence", "Tuple", "Integer", "Real", "Boolean", "String", "TupleType" };
