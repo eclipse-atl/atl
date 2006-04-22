0a81e2b891ad6f7dffee532e5e1004d9d1058967
fjouault <fjouault>
2006-04-22 16:35:22 +0000

    added ASMModel Navigation helpers (extracted from ebnfextractor)

---
+++ deprecated/org.atl.engine.vm/src/org/atl/engine/vm/nativelib/ASMModelNavigationException.java
@@ -0,0 +1,8 @@
+package org.atl.engine.vm.nativelib;
+
+public class ASMModelNavigationException extends RuntimeException {
+
+	public ASMModelNavigationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
