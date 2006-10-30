548e4b43c046b8c697c8a8fbf9b9b629498b08ad
mbohlen <mbohlen>
2006-10-30 19:06:09 +0000

    Changed into a deployment script for the jars which have no source code.

--- deprecated/org.atl.eclipse.engine/add-jars-to-maven.sh
+++ deprecated/org.atl.eclipse.engine/add-jars-to-maven.sh
@@ -1,5 +1,6 @@
 ## !/bin/sh
 
-mvn install:install-file -Dfile=lib/ebnfinjector.jar  -DgroupId=org.atl.eclipse.engine -DartifactId=ebnfinjector  -Dversion=1.13 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/ebnfextractor.jar -DgroupId=org.atl.eclipse.engine -DartifactId=ebnfextractor -Dversion=1.13 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/ATL-importer.jar  -DgroupId=org.atl.eclipse.engine -DartifactId=atl-importer  -Dversion=1.9  -Dpackaging=jar
+mvn deploy:deploy-file -DrepositoryId=andromda-repository -Durl=scp://galaxy.andromda.org/var/www/andromda/maven2 -Dfile=lib/ebnfinjector.jar  -DgroupId=org.atl.eclipse.engine -DartifactId=ebnfinjector  -Dversion=1.13 -Dpackaging=jar -DgeneratePom=true
+mvn deploy:deploy-file -DrepositoryId=andromda-repository -Durl=scp://galaxy.andromda.org/var/www/andromda/maven2 -Dfile=lib/ebnfextractor.jar -DgroupId=org.atl.eclipse.engine -DartifactId=ebnfextractor -Dversion=1.13 -Dpackaging=jar -DgeneratePom=true
+mvn deploy:deploy-file -DrepositoryId=andromda-repository -Durl=scp://galaxy.andromda.org/var/www/andromda/maven2 -Dfile=lib/ATL-importer.jar  -DgroupId=org.atl.eclipse.engine -DartifactId=atl-importer  -Dversion=1.9  -Dpackaging=jar -DgeneratePom=true
+
