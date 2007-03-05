02e903c9567ceaf0ac43f5961fed79b4dee6bdec
fallilaire <fallilaire>
2007-03-05 17:26:20 +0000

    deleted useless import

--- deprecated/org.atl.engine.repositories.emf4atl/src/org/atl/engine/repositories/emf4atl/ASMEMFModel.java
+++ deprecated/org.atl.engine.repositories.emf4atl/src/org/atl/engine/repositories/emf4atl/ASMEMFModel.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Random;
 import java.util.Set;
 
 import org.atl.engine.vm.ModelLoader;
