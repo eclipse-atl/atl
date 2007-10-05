54cb79434362a293008593d960d34015b42022fc
fallilaire <fallilaire>
2007-10-05 15:56:37 +0000

    added super as keyword

--- plugins/org.eclipse.m2m.atl.adt.editor/src/org/eclipse/m2m/atl/adt/ui/text/IAtlLexems.java
+++ plugins/org.eclipse.m2m.atl.adt.editor/src/org/eclipse/m2m/atl/adt/ui/text/IAtlLexems.java
@@ -10,7 +10,7 @@ public interface IAtlLexems {
 	
 	public final static String[] BRACKETS	= { "(", ")", "{", "}" };
 	public final static String[] CONSTANTS	= { "true", "false" };
-	public final static String[] KEYWORDS	= { "not", "and", "or", "xor", "implies", "module", "create", "from", "uses", "helper", "def", "context", "rule", "using", "derived", "to", "mapsTo", "distinct", "foreach", "in", "do", "if", "then", "else", "endif", "let", "library", "query", "for", "div", "refining", "entrypoint", "endpoint", "extends", "abstract", "unique", "lazy" };
+	public final static String[] KEYWORDS	= { "not", "and", "or", "xor", "implies", "module", "create", "from", "uses", "helper", "def", "context", "rule", "using", "derived", "to", "mapsTo", "distinct", "foreach", "in", "do", "if", "then", "else", "endif", "let", "library", "query", "for", "div", "refining", "entrypoint", "endpoint", "extends", "abstract", "unique", "lazy", "super" };
 	public final static String[] OPERATORS	= { ">", ".", "->", "-", "*", "/", "+", "=", ">", "<", ">=", "<=", "<>", "<-" };
 	public final static String[] SYMBOLS	= { "!", ",", ";", ":", "|" };
 	public final static String[] TYPES		= { "Bag", "Set", "OrderedSet", "Sequence", "Tuple", "Integer", "Real", "Boolean", "String", "TupleType" };
