fb23580142be20846bf59a4e33e94f0f4ea5390b
wpiers <wpiers>
2008-02-06 15:04:50 +0000

    removed old test outputter

--- tests/org.eclipse.m2m.atl.tests/src/org/eclipse/m2m/atl/tests/suite/AllTests.java
+++ tests/org.eclipse.m2m.atl.tests/src/org/eclipse/m2m/atl/tests/suite/AllTests.java
@@ -11,11 +11,8 @@
 package org.eclipse.m2m.atl.tests.suite;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 import junit.framework.Test;
@@ -25,11 +22,9 @@ import junit.textui.TestRunner;
 
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
-import org.eclipse.m2m.atl.tests.unit.TestNonRegressionParser;
 import org.eclipse.m2m.atl.tests.unit.TestNonRegressionTransfo;
 import org.eclipse.m2m.atl.tests.unit.atlvm.TestNonRegressionEMFVM;
 import org.eclipse.m2m.atl.tests.unit.atlvm.TestNonRegressionVM;
-import org.eclipse.m2m.atl.tests.util.FileUtils;
 
 /**
  * Launches all the JUnit tests for ATL.
@@ -49,7 +44,7 @@ public class AllTests extends TestCase implements IApplication {
 				emfvmresults.put(directory, time);			
 			}
 	}
-	
+
 	/**
 	 * Launches the test with the given arguments.
 	 * 
@@ -66,55 +61,13 @@ public class AllTests extends TestCase implements IApplication {
 	 * @return The testsuite containing all the tests
 	 */
 	public static Test suite() {
-		final TestSuite suite = new TestSuite("ATL test suite") {//$NON-NLS-1$
-			protected void finalize() throws Throwable {
-				File trace = new File(FileUtils.getTestCommonDirectory()+"\\trace.txt");
-				PrintWriter writer = new PrintWriter(new FileOutputStream(trace));
-				outputsResults(writer);
-				super.finalize();
-				outputsResults(null);
-				super.finalize();
-			}
-		};
+		final TestSuite suite = new TestSuite("ATL test suite");//$NON-NLS-1$	
 		suite.addTestSuite(TestNonRegressionEMFVM.class);
 		suite.addTestSuite(TestNonRegressionVM.class);
-		suite.addTestSuite(TestNonRegressionParser.class);
+		//suite.addTestSuite(TestNonRegressionParser.class);
 		return suite;
 	}
 
-	private static void outputsResults(PrintWriter writer) throws Exception {
-		if (writer != null) {
-
-			String localPath = FileUtils.getTestCommonDirectory();
-			final String htmlPath = "http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.m2m/org.eclipse.m2m.atl/tests/org.eclipse.m2m.atl.tests";//$NON-NLS-1$	
-
-			for (Iterator iterator = vmresults.keySet().iterator(); iterator.hasNext();) {
-				File directory = (File) iterator.next();
-				String path = directory.toString();
-				path = path.substring(localPath.length());
-				path = htmlPath+path.replaceAll("[/\\\\]+", "/")+"/?root=Modeling_Project";//$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$
-
-				writer.println("|-");//$NON-NLS-1$
-				writer.println("! colspan=1 | ["+path+" "+directory.getName()+"]");//$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$
-
-				if (emfvmresults.get(directory) != null) {
-					writer.println("! colspan=1 | <b style=\"color:green\">PASS</b>");//$NON-NLS-1$
-					writer.println("! colspan=1 | "+emfvmresults.get(directory)+"s.");//$NON-NLS-1$//$NON-NLS-2$
-				} else {
-
-					writer.println("! colspan=1 | <b style=\"color:red\">FAIL</b>");//$NON-NLS-1$
-					writer.println("! colspan=1 | ");//$NON-NLS-1$
-				}
-				writer.println("! colspan=1 | "+vmresults.get(directory)+"s.");//$NON-NLS-1$//$NON-NLS-2$
-				writer.println("! colspan=1 | ");//$NON-NLS-1$
-
-			}
-
-			writer.close();
-
-		}
-	}
-	
 	/**
 	 * {@inheritDoc}
 	 * 
