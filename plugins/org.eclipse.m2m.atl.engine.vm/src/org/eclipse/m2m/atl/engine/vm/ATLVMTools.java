9c6b7d3d00722ff2fb1c8493d1e126810877b8fd
wpiers <wpiers>
2008-09-23 09:53:28 +0000

    moved atlvmTools to regular vm plugin

---
+++ plugins/org.eclipse.m2m.atl.engine.vm/src/org/eclipse/m2m/atl/engine/vm/ATLVMTools.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2006 INRIA.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Frederic Jouault (INRIA) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.m2m.atl.engine.vm;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.eclipse.m2m.atl.engine.vm.nativelib.ASMOclType;
+
+/**
+ * ATL VM utilities.
+ * 
+ * @author <a href="mailto:frederic.jouault@univ-nantes.fr">Frederic Jouault</a>
+ */
+public abstract class ATLVMTools {
+
+	private ATLVMTools() {
+	}
+
+	/**
+	 * Converts an operation to a VM operation.
+	 * 
+	 * @param cl the class which contains the operation
+	 * @param name the operation name
+	 * @return the VM operation
+	 */
+	public static Operation toVMOperation(Class cl, String name) {
+		Operation ret = null;
+
+		Method m = null;
+
+		for (Iterator i = Arrays.asList(cl.getDeclaredMethods()).iterator(); i.hasNext() && (m == null);) {
+			Method me = (Method)i.next();
+			if (me.getName().equals(name)) {
+				m = me;
+			}
+		}
+
+		if (m != null) {
+			ret = new ClassNativeOperation(m);
+		}
+
+		return ret;
+	}
+
+	/**
+	 * Adds a VM operation to a context.
+	 * 
+	 * @param context the context
+	 * @param op operation
+	 */
+	public static void addVMOperation(ASMOclType context, Operation op) {
+		((Map)ASMOclType.getVMOperations().get(context)).put(op.getName(), op);
+	}
+}
