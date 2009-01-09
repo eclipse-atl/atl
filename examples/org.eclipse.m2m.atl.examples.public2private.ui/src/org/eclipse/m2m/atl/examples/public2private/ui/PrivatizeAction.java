9329796fc32984a49f0a9186901d70a9411ba5c5
wpiers <wpiers>
2009-01-09 11:40:59 +0000

    corrected/updated examples

--- examples/org.eclipse.m2m.atl.examples.public2private.ui/src/org/eclipse/m2m/atl/examples/public2private/ui/PrivatizeAction.java
+++ examples/org.eclipse.m2m.atl.examples.public2private.ui/src/org/eclipse/m2m/atl/examples/public2private/ui/PrivatizeAction.java
@@ -109,7 +109,7 @@ public class PrivatizeAction implements IObjectActionDelegate {
 
 		// Metamodels
 		umlMetamodel = factory.newReferenceModel();
-		injector.inject(umlMetamodel, "uri:http://www.declipse.org/uml2/2.1.0/UML"); //$NON-NLS-1$
+		injector.inject(umlMetamodel, "uri:http://www.eclipse.org/uml2/2.1.0/UML"); //$NON-NLS-1$
 		refiningTraceMetamodel = factory.getBuiltInResource("RefiningTrace"); //$NON-NLS-1$
 		
 		// Getting launcher
