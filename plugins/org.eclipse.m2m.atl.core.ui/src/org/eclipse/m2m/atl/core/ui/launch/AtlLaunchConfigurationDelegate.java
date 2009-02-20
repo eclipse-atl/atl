97b8a8d4f0b2d1b4952a7ac1db786f888c9b3e3e
wpiers <wpiers>
2009-02-20 13:14:24 +0000

    corrected test, prevent NPE

--- plugins/org.eclipse.m2m.atl.core.ui/src/org/eclipse/m2m/atl/core/ui/launch/AtlLaunchConfigurationDelegate.java
+++ plugins/org.eclipse.m2m.atl.core.ui/src/org/eclipse/m2m/atl/core/ui/launch/AtlLaunchConfigurationDelegate.java
@@ -63,7 +63,7 @@ public class AtlLaunchConfigurationDelegate implements ILaunchConfigurationDeleg
 		boolean isRefining = configuration.getAttribute(ATLLaunchConstants.IS_REFINING, false);
 
 		Map<String, Object> options = new HashMap<String, Object>();
-		boolean isRefiningTraceMode = atlCompiler.equals("atl2006") && isRefining; //$NON-NLS-1$
+		boolean isRefiningTraceMode = "atl2006".equals(atlCompiler) && isRefining; //$NON-NLS-1$
 		options.put("isRefiningTraceMode", new Boolean(isRefiningTraceMode).toString()); //$NON-NLS-1$
 		options.put("launch", launch); //$NON-NLS-1$
 		options.put("monitor", monitor); //$NON-NLS-1$
