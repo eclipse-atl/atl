c9f0540120bad576fdec962210b516359210031c
wpiers <wpiers>
2009-04-16 09:50:12 +0000

    added tcs wrapper for new api

---
+++ plugins/org.eclipse.m2m.atl.dsls/src/org/eclipse/m2m/atl/dsls/core/EMFTCSExtractor.java
@@ -0,0 +1,161 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.m2m.atl.dsls.core;
+
+import java.io.OutputStream;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.emf.ecore.EEnumLiteral;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.gmt.tcs.extractor.ModelAdapter;
+import org.eclipse.gmt.tcs.extractor.PrettyPrinter;
+import org.eclipse.gmt.tcs.extractor.TCSExtractor;
+import org.eclipse.m2m.atl.core.IModel;
+import org.eclipse.m2m.atl.core.emf.EMFModel;
+import org.eclipse.m2m.atl.core.emf.EMFReferenceModel;
+
+/**
+ * A wrapper which allow to generate text from {@link IModel} with TCS.
+ * 
+ * @author <a href="mailto:william.piers@obeo.fr">William Piers</a>
+ */
+public class EMFTCSExtractor extends TCSExtractor {
+
+	public EMFTCSExtractor() {
+		super();
+	}
+
+	public void extract(EMFModel source, OutputStream target, Map params) {
+		new PrettyPrinter().prettyPrint(source, new EMFExtractorAdapter(), target, params);
+	}
+
+	public class EMFExtractorAdapter implements ModelAdapter {
+
+		public Object get(Object modelElement, String name) {
+			if (modelElement == null) {
+				return null;
+			} else {
+				EObject eo = (EObject)modelElement;
+				EStructuralFeature sf = eo.eClass().getEStructuralFeature(name);
+				if (sf == null) {
+					return null;
+				}
+				return eo.eGet(sf);
+			}
+		}
+
+		public boolean getBool(Object me, String propName) {
+			boolean ret = false;
+			try {
+				ret = ((Boolean)get(me, propName)).booleanValue();
+			} catch (Exception e) {
+				throw new RuntimeException("could not read property \"" + propName + "\" of element " + me
+						+ " : " + getTypeName(me), e);
+			}
+			return ret;
+		}
+
+		public boolean getBoolUndefinedIsFalse(Object me, String propName) {
+			boolean ret = false;
+			Object v = get(me, propName);
+			if (v instanceof Boolean)
+				ret = ((Boolean)v).booleanValue();
+
+			return ret;
+		}
+
+		public Iterator getCol(Object me, String propName) {
+			return ((Collection)get(me, propName)).iterator();
+		}
+
+		public Set getElementsByType(Object model, String typeName) {
+			EMFReferenceModel metamodel = ((EMFModel)model).getReferenceModel();
+			return ((EMFModel)model).getElementsByType(metamodel.getMetaElementByName(typeName));
+		}
+
+		public int getInt(Object me, String propName) {
+			return ((Integer)get(me, propName)).intValue();
+		}
+
+		public Object getME(Object me, String propName) {
+			return get(me, propName);
+		}
+
+		public Object getMetaobject(Object me) {
+			return ((EObject)me).eClass();
+		}
+
+		public String getString(Object me, String propName) {
+			String ret = null;
+			Object v = get(me, propName);
+
+			if (v != null) {
+				if ((v instanceof Collection) && (((Collection)v).size() == 1)) {
+					v = ((Collection)v).iterator().next();
+				}
+
+				if (v instanceof String) {
+					ret = (String)v;
+				} else if (v instanceof EEnumLiteral) {
+					ret = ((EEnumLiteral)v).getName();
+				}
+			}
+
+			return ret;
+		}
+
+		public String getTypeName(Object me) {
+			return getName(((EObject)me).eClass());
+		}
+
+		public String getName(Object ame) {
+			return getString(ame, "name");
+		}
+
+		public Object refImmediateComposite(Object me) {
+			return ((EObject)me).eContainer();
+		}
+
+		public boolean isAModelElement(Object o) {
+			return (o instanceof EObject);
+		}
+
+		public Object getPropertyType(Object f, String propName) {
+			Object res = get(f, propName);
+			if (res instanceof EObject) {
+				return get(res, "eType");
+			}
+			return null;
+		}
+
+		public boolean isPrimitive(Object value) {
+			return ((value instanceof String) || (value instanceof Boolean) || (value instanceof Double) || (value instanceof Integer));
+		}
+
+		public boolean isEnumLiteral(Object value) {
+			return (value instanceof EEnumLiteral);
+		}
+
+		public String getEnumLiteralName(Object me) {
+			return getName(me).toString();
+		}
+
+		public String nextString(Iterator i) {
+			return i.next().toString();
+		}
+
+	}
+
+}
---
+++ plugins/org.eclipse.m2m.atl.dsls/src/org/eclipse/m2m/atl/dsls/core/EMFTCSInjector.java
@@ -0,0 +1,215 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.m2m.atl.dsls.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EEnum;
+import org.eclipse.emf.ecore.EEnumLiteral;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.impl.EcoreFactoryImpl;
+import org.eclipse.gmt.tcs.injector.ModelAdapter;
+import org.eclipse.gmt.tcs.injector.ParserLauncher;
+import org.eclipse.gmt.tcs.injector.TCSInjector;
+import org.eclipse.m2m.atl.common.ATLLogger;
+import org.eclipse.m2m.atl.core.IModel;
+import org.eclipse.m2m.atl.core.emf.EMFModel;
+
+/**
+ * A wrapper which allow to create {@link IModel} from text with TCS.
+ * 
+ * @author <a href="mailto:william.piers@obeo.fr">William Piers</a>
+ */
+public class EMFTCSInjector extends TCSInjector {
+
+	public EMFTCSInjector() {
+		super();
+	}
+
+	public Object inject(EMFModel target, InputStream source, Map params) throws IOException {
+		ModelAdapter targetModelAdapter = new EMFInjectorAdapter(target);
+
+		EMFModel problems = (EMFModel)params.get("problems");
+		if (problems != null) {
+			ModelAdapter problemsModelAdapter = new EMFInjectorAdapter(problems);
+			params.put("problems", problemsModelAdapter);
+		}
+
+		EObject root = (EObject)new ParserLauncher().parse(targetModelAdapter, source, params);
+		target.commitToResource();
+		problems.commitToResource();
+		return root;
+	}
+
+	public class EMFInjectorAdapter implements ModelAdapter {
+
+		private EMFModel model;
+
+		public EMFInjectorAdapter(Object model) {
+			this.model = (EMFModel)model;
+		}
+
+		public Object getModel() {
+			return this.model;
+		}
+
+		public Object get(Object modelElement, String name) {
+			if (modelElement == null) {
+				return null;
+			} else {
+				EObject eo = (EObject)modelElement;
+				EStructuralFeature sf = eo.eClass().getEStructuralFeature(name);
+				if (sf == null) {
+					return null;
+				}
+				return eo.eGet(sf);
+			}
+		}
+
+		public Object createElement(String typeName) {
+			return model.newElement(getTypeByName(typeName));
+		}
+
+		public Set getElementsByType(String typeName) {
+			return model.getElementsByType(getTypeByName(typeName));
+		}
+
+		public void set(Object modelElement, String name, Object value) {
+			if (value == null) {
+				return;
+			}
+
+			final EObject eo = (EObject)modelElement;
+
+			final EStructuralFeature feature = eo.eClass().getEStructuralFeature(name);
+
+			if (feature == null) {
+				return;
+			}
+
+			// makes it possible to use an integer to set a floating point property
+			if (value instanceof Integer) {
+				String targetType = feature.getEType().getInstanceClassName();
+				if ("java.lang.Double".equals(targetType) || "java.lang.Float".equals(targetType)) { //$NON-NLS-1$ //$NON-NLS-2$
+					value = new Double(((Integer)value).doubleValue());
+				}
+			}
+
+			EClassifier type = feature.getEType();
+			boolean targetIsEnum = type instanceof EEnum;
+
+			Object oldValue = eo.eGet(feature);
+			if (oldValue instanceof Collection) {
+				Collection oldCol = (Collection)oldValue;
+				if (value instanceof Collection) {
+					if (targetIsEnum) {
+						EEnum eenum = (EEnum)type;
+						for (Iterator i = ((Collection)value).iterator(); i.hasNext();) {
+							Object v = i.next();
+							oldCol.add(eenum.getEEnumLiteralByLiteral(v.toString()).getInstance());
+						}
+					} else {
+						oldCol.addAll((Collection)value);
+					}
+				} else {
+					if (targetIsEnum) {
+						EEnum eenum = (EEnum)type;
+						oldCol.add(eenum.getEEnumLiteralByLiteral(value.toString()).getInstance());
+					} else {
+						oldCol.add(value);
+					}
+				}
+			} else {
+				if (value instanceof Collection) {
+					Collection c = (Collection)value;
+					if (!c.isEmpty()) {
+						value = c.iterator().next();
+					} else {
+						value = null;
+					}
+				}
+				if (targetIsEnum) {
+					EEnum eenum = (EEnum)type;
+					if (value != null) {
+						EEnumLiteral literal = eenum.getEEnumLiteral(value.toString());
+						if (literal != null) {
+							eo.eSet(feature, literal.getInstance());
+						} else {
+							return;
+						}
+					}
+				} else {
+					eo.eSet(feature, value);
+				}
+			}
+		}
+
+		public boolean isCandidate(Object ame, String typeName) {
+			boolean ret = false;
+			Object valueType = getTypeByName(typeName);
+
+			Object type = getType(ame);
+
+			if (valueType instanceof EObject) {
+				final EObject o = (EObject)valueType;
+				final EObject t = (EObject)type;
+
+				if ((o instanceof EClass) && (t instanceof EClass)) {
+					try {
+						ret = o.equals(t) || ((EClass)o).isSuperTypeOf((EClass)t);
+					} catch (Exception e) {
+						ATLLogger.log(Level.SEVERE, e.getLocalizedMessage(), e);
+					}
+				}
+			}
+			return ret;
+		}
+
+		public Object getType(Object value) {
+			if (value instanceof EObject) {
+				return ((EObject)value).eClass();
+			} else if (value instanceof EList) {
+				return ArrayList.class;
+			} else {
+				return value.getClass();
+			}
+		}
+
+		public boolean isAModelElement(Object me) {
+			return me instanceof EObject;
+		}
+
+		public String getString(Object ame, String propName) {
+			return get(ame, propName).toString();
+		}
+
+		public Object createEnumLiteral(String name) {
+			EEnumLiteral ret = EcoreFactoryImpl.eINSTANCE.createEEnumLiteral();
+			ret.setName(name);
+			return ret;
+		}
+
+		private Object getTypeByName(String typeName) {
+			return model.getReferenceModel().getMetaElementByName(typeName);
+		}
+	}
+}
