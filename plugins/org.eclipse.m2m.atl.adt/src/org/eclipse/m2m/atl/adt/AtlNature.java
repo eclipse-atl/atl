8ca0c04b26254e1f57fb72418b35797e95b5f2c8
wpiers <wpiers>
2009-04-29 12:31:45 +0000

    corrected nature toggle

--- plugins/org.eclipse.m2m.atl.adt/src/org/eclipse/m2m/atl/adt/AtlNature.java
+++ plugins/org.eclipse.m2m.atl.adt/src/org/eclipse/m2m/atl/adt/AtlNature.java
@@ -28,69 +28,61 @@ public class AtlNature implements IProjectNature {
 
 	private IProject project;
 
-	/**{@inheritDoc}
-	 *
+	/**
+	 * {@inheritDoc}
+	 * 
 	 * @see org.eclipse.core.resources.IProjectNature#configure()
 	 */
 	public void configure() throws CoreException {
 		IProjectDescription desc = project.getDescription();
 		ICommand[] commands = desc.getBuildSpec();
-		boolean found = false;
 
 		for (int i = 0; i < commands.length; ++i) {
 			if (commands[i].getBuilderName().equals(AtlBuilder.ATL_BUILDER_ID)) {
-				found = true;
-				break;
+				return;
 			}
 		}
-		if (!found) {
-			// add builder to project
-			ICommand command = desc.newCommand();
-			command.setBuilderName(AtlBuilder.ATL_BUILDER_ID);
-			ICommand[] newCommands = new ICommand[commands.length + 1];
 
-			// Add it before other builders.
-			System.arraycopy(commands, 0, newCommands, 1, commands.length);
-			newCommands[0] = command;
-			desc.setBuildSpec(newCommands);
-			project.setDescription(desc, null);
-		}
+		ICommand[] newCommands = new ICommand[commands.length + 1];
+		System.arraycopy(commands, 0, newCommands, 0, commands.length);
+		ICommand command = desc.newCommand();
+		command.setBuilderName(AtlBuilder.ATL_BUILDER_ID);
+		newCommands[newCommands.length - 1] = command;
+		desc.setBuildSpec(newCommands);
+		project.setDescription(desc, null);
 	}
 
-	/**{@inheritDoc}
-	 *
+	/**
+	 * {@inheritDoc}
+	 * 
 	 * @see org.eclipse.core.resources.IProjectNature#deconfigure()
 	 */
 	public void deconfigure() throws CoreException {
-		IProjectDescription desc = project.getDescription();
-		ICommand[] commands = desc.getBuildSpec();
-		boolean found = false;
-		int i = 0;
-		for ( ; i < commands.length; ++i) {
+		IProjectDescription description = getProject().getDescription();
+		ICommand[] commands = description.getBuildSpec();
+		for (int i = 0; i < commands.length; ++i) {
 			if (commands[i].getBuilderName().equals(AtlBuilder.ATL_BUILDER_ID)) {
-				found = true;
-				break;
-			}
-		}
-		if (found) {
-			ICommand[] newCommands = new ICommand[commands.length - 1];
-			System.arraycopy(commands, 0, newCommands, 0, i);
-			if (i + 1 < commands.length) {
-				System.arraycopy(commands, i + 1, newCommands, i + 1, commands.length - (i + 1));
+				ICommand[] newCommands = new ICommand[commands.length - 1];
+				System.arraycopy(commands, 0, newCommands, 0, i);
+				System.arraycopy(commands, i + 1, newCommands, i, commands.length - i - 1);
+				description.setBuildSpec(newCommands);
+				return;
 			}
 		}
 	}
 
-	/**{@inheritDoc}
-	 *
+	/**
+	 * {@inheritDoc}
+	 * 
 	 * @see org.eclipse.core.resources.IProjectNature#getProject()
 	 */
 	public IProject getProject() {
 		return project;
 	}
 
-	/**{@inheritDoc}
-	 *
+	/**
+	 * {@inheritDoc}
+	 * 
 	 * @see org.eclipse.core.resources.IProjectNature#setProject(org.eclipse.core.resources.IProject)
 	 */
 	public void setProject(IProject project) {
