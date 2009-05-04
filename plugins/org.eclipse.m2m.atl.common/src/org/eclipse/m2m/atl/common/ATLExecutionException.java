1a4056a1daa153c26e2db7ff805303349a832dea
wpiers <wpiers>
2009-05-04 15:25:19 +0000

    improved error messages

--- plugins/org.eclipse.m2m.atl.common/src/org/eclipse/m2m/atl/common/ATLExecutionException.java
+++ plugins/org.eclipse.m2m.atl.common/src/org/eclipse/m2m/atl/common/ATLExecutionException.java
@@ -41,11 +41,4 @@ public abstract class ATLExecutionException extends RuntimeException {
 		super(message, cause);
 	}
 
-	/**
-	 * Returns the name of the module where the error occurred.
-	 * 
-	 * @return the name of the module where the error occurred
-	 */
-	public abstract String getModuleName();
-
 }
