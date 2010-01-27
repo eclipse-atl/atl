4acd737cf0474e6ee430f8b25ff4214c17027c68
wpiers <wpiers>
2010-01-27 14:27:59 +0000

    added properties comments

--- plugins/org.eclipse.m2m.atl.adt/src-gen/org/eclipse/m2m/atl/adt/runner/CreateModulePropertiesWriter.java
+++ plugins/org.eclipse.m2m.atl.adt/src-gen/org/eclipse/m2m/atl/adt/runner/CreateModulePropertiesWriter.java
@@ -15,20 +15,25 @@ public class CreateModulePropertiesWriter
   }
 
   public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
-  protected final String TEXT_1 = "";
-  protected final String TEXT_2 = ".modules=";
-  protected final String TEXT_3 = ",";
-  protected final String TEXT_4 = NL;
-  protected final String TEXT_5 = ".metamodels.";
-  protected final String TEXT_6 = "=";
+  protected final String TEXT_1 = "# ==============================================================================" + NL + "# ";
+  protected final String TEXT_2 = " properties" + NL + "# ==============================================================================" + NL + "" + NL + "# ATL modules: if several, by order of superimposition (the latter ones overrides the former ones)";
+  protected final String TEXT_3 = NL;
+  protected final String TEXT_4 = ".modules = ";
+  protected final String TEXT_5 = ",";
+  protected final String TEXT_6 = NL + NL + "# Metamodels paths or nsUris";
   protected final String TEXT_7 = NL;
-  protected final String TEXT_8 = ".libraries.";
-  protected final String TEXT_9 = "=";
-  protected final String TEXT_10 = NL;
-  protected final String TEXT_11 = ".options.";
-  protected final String TEXT_12 = "=";
-  protected final String TEXT_13 = NL;
-  protected final String TEXT_14 = NL;
+  protected final String TEXT_8 = ".metamodels.";
+  protected final String TEXT_9 = " = ";
+  protected final String TEXT_10 = NL + NL + "# Libraries paths";
+  protected final String TEXT_11 = NL;
+  protected final String TEXT_12 = ".libraries.";
+  protected final String TEXT_13 = " = ";
+  protected final String TEXT_14 = NL + NL + "# ATL Launching options";
+  protected final String TEXT_15 = NL;
+  protected final String TEXT_16 = ".options.";
+  protected final String TEXT_17 = " = ";
+  protected final String TEXT_18 = NL;
+  protected final String TEXT_19 = NL;
 
   public String generate(Object argument)
   {
@@ -39,42 +44,48 @@ public class CreateModulePropertiesWriter
     stringBuffer.append(TEXT_1);
     stringBuffer.append(content.getClassShortName());
     stringBuffer.append(TEXT_2);
+    stringBuffer.append(TEXT_3);
+    stringBuffer.append(content.getClassShortName());
+    stringBuffer.append(TEXT_4);
     for (int i = 0; i < content.getTransformationFiles().length; i++) {
     stringBuffer.append(content.getTransformationFiles()[i].getName());
     if (i < content.getTransformationFiles().length -1 ) {
-    stringBuffer.append(TEXT_3);
-    }
-    }
-    for (String metamodelName : content.getAllMetamodelsNames()) { String location = content.getMetamodelLocations().get(metamodelName); 
-    stringBuffer.append(TEXT_4);
-    stringBuffer.append(content.getClassShortName());
     stringBuffer.append(TEXT_5);
-    stringBuffer.append(metamodelName);
+    }
+    }
     stringBuffer.append(TEXT_6);
+    for (String metamodelName : content.getAllMetamodelsNames()) { String location = content.getMetamodelLocations().get(metamodelName); 
+    stringBuffer.append(TEXT_7);
+    stringBuffer.append(content.getClassShortName());
+    stringBuffer.append(TEXT_8);
+    stringBuffer.append(metamodelName);
+    stringBuffer.append(TEXT_9);
     if (location != null) {
     stringBuffer.append(location);
     }
     }
+    stringBuffer.append(TEXT_10);
     for (String libraryName : content.getAllLibrariesNames()) { String location = content.getLibraryLocations().get(libraryName); 
-    stringBuffer.append(TEXT_7);
+    stringBuffer.append(TEXT_11);
     stringBuffer.append(content.getClassShortName());
-    stringBuffer.append(TEXT_8);
+    stringBuffer.append(TEXT_12);
     stringBuffer.append(libraryName);
-    stringBuffer.append(TEXT_9);
+    stringBuffer.append(TEXT_13);
     if (location != null) {
     stringBuffer.append(new Path(location).lastSegment());
     }
     }
+    stringBuffer.append(TEXT_14);
     for (Entry<String, String> entry : content.getOptions().entrySet()) {
-    stringBuffer.append(TEXT_10);
+    stringBuffer.append(TEXT_15);
     stringBuffer.append(content.getClassShortName());
-    stringBuffer.append(TEXT_11);
+    stringBuffer.append(TEXT_16);
     stringBuffer.append(entry.getKey());
-    stringBuffer.append(TEXT_12);
+    stringBuffer.append(TEXT_17);
     stringBuffer.append(entry.getValue());
     }
-    stringBuffer.append(TEXT_13);
-    stringBuffer.append(TEXT_14);
+    stringBuffer.append(TEXT_18);
+    stringBuffer.append(TEXT_19);
     return stringBuffer.toString();
   }
 }
