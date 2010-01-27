17618e4ed1b509230ca57f20edf643ee0c17d164
wpiers <wpiers>
2010-01-27 14:27:36 +0000

    deactivated launch config converter

--- plugins/org.eclipse.m2m.atl.adt.ui/src/org/eclipse/m2m/atl/adt/ui/compatibility/CompatibilityDialog.java
+++ plugins/org.eclipse.m2m.atl.adt.ui/src/org/eclipse/m2m/atl/adt/ui/compatibility/CompatibilityDialog.java
@@ -37,7 +37,6 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
 
 /**
  * An UI class for old ATL projects conversion.
@@ -48,7 +47,7 @@ public class CompatibilityDialog extends TitleAreaDialog {
 
 	private CheckboxTableViewer fProjectsTable;
 
-	private CheckboxTableViewer fConfTable;
+//	private CheckboxTableViewer fConfTable;
 
 	/**
 	 * Create an instance of this Dialog.
@@ -129,44 +128,44 @@ public class CompatibilityDialog extends TitleAreaDialog {
 			}
 		});
 
-		// Configuration tab
-		Composite confComposite = new Composite(tabFolder, SWT.NONE);
-		confComposite.setLayout(new GridLayout(2, false));
-		confComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
-		TabItem confItem = new TabItem(tabFolder, SWT.NONE);
-		confItem.setText(Messages.getString("CompatibilityDialog.6")); //$NON-NLS-1$
-		confItem.setControl(confComposite);
-
-		// creates the conf table
-		fConfTable = CheckboxTableViewer
-				.newCheckList(confComposite, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
-		fConfTable.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
-		ConfContentProvider confProvider = new ConfContentProvider();
-		fConfTable.setContentProvider(confProvider);
-		fConfTable.setLabelProvider(new ConfLabelProvider());
-		fConfTable.setInput(confProvider);
-
-		// create select/deselect commands
-		Composite confButtonsGroup = new Composite(confComposite, SWT.MAX);
-		confButtonsGroup.setLayout(new GridLayout());
-
-		Button confSelectAll = new Button(confButtonsGroup, SWT.CENTER);
-		confSelectAll.setText(Messages.getString("CompatibilityDialog.7")); //$NON-NLS-1$
-		confSelectAll.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				fConfTable.setAllChecked(true);
-			}
-		});
-
-		Button confDeselectAll = new Button(confButtonsGroup, SWT.CENTER);
-		confDeselectAll.setText(Messages.getString("CompatibilityDialog.8")); //$NON-NLS-1$
-		confDeselectAll.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				fConfTable.setAllChecked(false);
-			}
-		});
+//		// Configuration tab
+//		Composite confComposite = new Composite(tabFolder, SWT.NONE);
+//		confComposite.setLayout(new GridLayout(2, false));
+//		confComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+//		TabItem confItem = new TabItem(tabFolder, SWT.NONE);
+//		confItem.setText(Messages.getString("CompatibilityDialog.6")); //$NON-NLS-1$
+//		confItem.setControl(confComposite);
+//
+//		// creates the conf table
+//		fConfTable = CheckboxTableViewer
+//				.newCheckList(confComposite, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+//		fConfTable.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+//		ConfContentProvider confProvider = new ConfContentProvider();
+//		fConfTable.setContentProvider(confProvider);
+//		fConfTable.setLabelProvider(new ConfLabelProvider());
+//		fConfTable.setInput(confProvider);
+//
+//		// create select/deselect commands
+//		Composite confButtonsGroup = new Composite(confComposite, SWT.MAX);
+//		confButtonsGroup.setLayout(new GridLayout());
+//
+//		Button confSelectAll = new Button(confButtonsGroup, SWT.CENTER);
+//		confSelectAll.setText(Messages.getString("CompatibilityDialog.7")); //$NON-NLS-1$
+//		confSelectAll.addSelectionListener(new SelectionAdapter() {
+//			@Override
+//			public void widgetSelected(SelectionEvent e) {
+//				fConfTable.setAllChecked(true);
+//			}
+//		});
+//
+//		Button confDeselectAll = new Button(confButtonsGroup, SWT.CENTER);
+//		confDeselectAll.setText(Messages.getString("CompatibilityDialog.8")); //$NON-NLS-1$
+//		confDeselectAll.addSelectionListener(new SelectionAdapter() {
+//			@Override
+//			public void widgetSelected(SelectionEvent e) {
+//				fConfTable.setAllChecked(false);
+//			}
+//		});
 
 		return tabFolder;
 	}
@@ -180,13 +179,13 @@ public class CompatibilityDialog extends TitleAreaDialog {
 	protected void okPressed() {
 		try {
 			CompatibilityUtils.convertProjects(fProjectsTable.getCheckedElements());
-			if (CompatibilityUtils.convertConfigurations(fConfTable.getCheckedElements())) {
-				// need to restart eclipse
-				boolean restart = RestartDialog.openQuestion(this.getShell(), true);
-				if (restart) {
-					PlatformUI.getWorkbench().restart();
-				}
-			}
+			// if (CompatibilityUtils.convertConfigurations(fConfTable.getCheckedElements())) {
+			// // need to restart eclipse
+			// boolean restart = RestartDialog.openQuestion(this.getShell(), true);
+			// if (restart) {
+			// PlatformUI.getWorkbench().restart();
+			// }
+			// }
 		} catch (CoreException e) {
 			ATLLogger.log(Level.SEVERE, e.getLocalizedMessage(), e);
 		}
