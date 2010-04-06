725ed700d89dfe3d38c2e44e3ddd5754c8a4a5b9
wpiers <wpiers>
2010-04-06 15:27:39 +0000

    [308218] improved syntax colors

--- plugins/org.eclipse.m2m.atl.common/src/org/eclipse/m2m/atl/common/IAtlLexems.java
+++ plugins/org.eclipse.m2m.atl.common/src/org/eclipse/m2m/atl/common/IAtlLexems.java
@@ -35,6 +35,15 @@ public interface IAtlLexems {
 
 	/** Types. */
 	String[] TYPES = {
-			"Bag", "Set", "OrderedSet", "Sequence", "Tuple", "Integer", "Real", "Boolean", "String", "TupleType",}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$ //$NON-NLS-10$
+			"Bag", "Set", "OrderedSet", "Sequence", "Tuple", "Integer", "Real", "Boolean", "String", "TupleType", "Map", "Collection", "OclAny",}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$ //$NON-NLS-10$ //$NON-NLS-11$ //$NON-NLS-12$ //$NON-NLS-13$
+
+	/** Abstract types. */
+	String[] ABSTRACT_TYPES = {"Collection", "OclAny",}; //$NON-NLS-1$ //$NON-NLS-2$
+
+	/** Special comments. */
+	String[] SPECIAL_COMMENTS = {"-- @path", "-- @nsURI", "-- @lib", "-- @atlcompiler",}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+
+	/** Context keywords. */
+	String[] CONTEXT_KEYWORDS = {"self", "thisModule",}; //$NON-NLS-1$ //$NON-NLS-2$
 
 }
