c40fabf4a060313254ea390611beb02c37cb2e13
wpiers <wpiers>
2010-04-20 14:25:42 +0000

    fixed checkstyle bugs

--- plugins/org.eclipse.m2m.atl.adt/src/org/eclipse/m2m/atl/adt/AtlBuildVisitor.java
+++ plugins/org.eclipse.m2m.atl.adt/src/org/eclipse/m2m/atl/adt/AtlBuildVisitor.java
@@ -26,7 +26,6 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.m2m.atl.adt.runner.ATLProperties;
