e070eef332938460c450558753f0556be8c98ab4
dwagelaar <dwagelaar>
2011-06-22 07:14:05 +0000

    Initial commit.

---
+++ plugins/org.eclipse.m2m.atl.emftvm.trace.tests/src/org/eclipse/m2m/atl/emftvm/trace/tests/TargetElementTest.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Vrije Universiteit Brussel.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Dennis Wagelaar, Vrije Universiteit Brussel - initial API and
+ *         implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.m2m.atl.emftvm.trace.tests;
+
+import org.eclipse.m2m.atl.emftvm.trace.TargetElement;
+import org.eclipse.m2m.atl.emftvm.trace.TraceFactory;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Target Element</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class TargetElementTest extends TraceElementTest {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(TargetElementTest.class);
+	}
+
+	/**
+	 * Constructs a new Target Element test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public TargetElementTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Returns the fixture for this Target Element test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected TargetElement getFixture() {
+		return (TargetElement)fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(TraceFactory.eINSTANCE.createTargetElement());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //TargetElementTest
