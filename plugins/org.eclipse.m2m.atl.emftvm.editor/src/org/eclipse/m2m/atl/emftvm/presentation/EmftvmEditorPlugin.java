ebe9be9ab1b2336c5cec9717317caa6b1f5a0497
dwagelaar <dwagelaar>
2011-07-13 08:32:48 +0000

    Enable termination of a running EMFTVM, and clean up imports.

--- plugins/org.eclipse.m2m.atl.emftvm.editor/src/org/eclipse/m2m/atl/emftvm/presentation/EmftvmEditorPlugin.java
+++ plugins/org.eclipse.m2m.atl.emftvm.editor/src/org/eclipse/m2m/atl/emftvm/presentation/EmftvmEditorPlugin.java
@@ -12,11 +12,8 @@
 package org.eclipse.m2m.atl.emftvm.presentation;
 
 import org.eclipse.emf.common.EMFPlugin;
-
 import org.eclipse.emf.common.ui.EclipseUIPlugin;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
 import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
 
 /**
