e63cdd4da9408186a9982a97451b28db23b2e622
dwagelaar <dwagelaar>
2011-12-08 09:49:36 +0000

    Moved from plugins to tests.

---
+++ tests/org.eclipse.m2m.atl.emftvm.trace.tests/src/org/eclipse/m2m/atl/emftvm/trace/tests/TraceElementTest.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Vrije Universiteit Brussel.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Dennis Wagelaar, Vrije Universiteit Brussel - initial API and
+ *         implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.m2m.atl.emftvm.trace.tests;
+
+import junit.framework.TestCase;
+
+import org.eclipse.m2m.atl.emftvm.trace.TraceElement;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Element</b></em>'.
+ * @author <a href="mailto:dennis.wagelaar@vub.ac.be">Dennis Wagelaar</a>
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public abstract class TraceElementTest extends TestCase {
+
+	/**
+	 * The fixture for this Element test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected TraceElement fixture = null;
+
+	/**
+	 * Constructs a new Element test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public TraceElementTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Element test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(TraceElement fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Element test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected TraceElement getFixture() {
+		return fixture;
+	}
+
+} //TraceElementTest
