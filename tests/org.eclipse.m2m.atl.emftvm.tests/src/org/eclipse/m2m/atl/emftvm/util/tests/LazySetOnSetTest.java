2eaceb3b881ed0273dc9a98175036e5a5bdf5a2a
dwagelaar <dwagelaar@gmail.com>
2012-10-17 21:28:03 +0200

    Added tests for LazyBagOnCollection/LazySetOnSet.

---
+++ tests/org.eclipse.m2m.atl.emftvm.tests/src/org/eclipse/m2m/atl/emftvm/util/tests/LazySetOnSetTest.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Dennis Wagelaar.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Dennis Wagelaar - initial API and
+ *         implementation and/or initial documentation
+ *******************************************************************************/
+
+package org.eclipse.m2m.atl.emftvm.util.tests;
+
+import org.eclipse.m2m.atl.emftvm.util.LazySetOnSet;
+
+/**
+ * Tests {@link LazySetOnSet}.
+ * 
+ * @author <a href="mailto:dwagelaar@gmail.com">Dennis Wagelaar</a>
+ */
+public class LazySetOnSetTest extends LazySetTest {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected LazySetOnSet<String> getTestLazyCollection() {
+		// Don't reuse lazy collections because their cache takes over when it
+		// is complete
+		return new LazySetOnSet<String>(getDataSource());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected <T> LazySetOnSet<T> getEmptyLazyCollection() {
+		return new LazySetOnSet<T>(null);
+	}
+
+}
