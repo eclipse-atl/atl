e2f9f08e04af76a0bb9ef8ef7503e41b934a4cdf
dwagelaar <dwagelaar@gmail.com>
2013-05-28 21:30:43 +0200

    Use BufferedOutputStream in compiler, and close streams.

--- plugins/org.eclipse.m2m.atl.emftvm.compiler/src/org/eclipse/m2m/atl/emftvm/compiler/AtlToEmftvmCompiler.java
+++ plugins/org.eclipse.m2m.atl.emftvm.compiler/src/org/eclipse/m2m/atl/emftvm/compiler/AtlToEmftvmCompiler.java
@@ -11,6 +11,7 @@
  *******************************************************************************/
 package org.eclipse.m2m.atl.emftvm.compiler;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -52,9 +53,9 @@ import org.eclipse.m2m.atl.engine.compiler.AtlStandaloneCompiler;
 import org.eclipse.m2m.atl.engine.compiler.CompileTimeError;
 import org.eclipse.m2m.atl.engine.parser.AtlParser;
 
-
 /**
  * Invokes the ATL to EMFTVM compiler.
+ * 
  * @author <a href="mailto:dennis.wagelaar@vub.ac.be">Dennis Wagelaar</a>
  */
 public class AtlToEmftvmCompiler implements AtlStandaloneCompiler {
@@ -72,8 +73,8 @@ public class AtlToEmftvmCompiler implements AtlStandaloneCompiler {
 		super();
 
 		final Metamodel atlmm = EmftvmFactory.eINSTANCE.createMetamodel();
-		atlmm.setResource(((EMFReferenceModel)AtlParser.getDefault().getAtlMetamodel()).getResource());
-		pbmm.setResource(((EMFReferenceModel)AtlParser.getDefault().getProblemMetamodel()).getResource());
+		atlmm.setResource(((EMFReferenceModel) AtlParser.getDefault().getAtlMetamodel()).getResource());
+		pbmm.setResource(((EMFReferenceModel) AtlParser.getDefault().getProblemMetamodel()).getResource());
 		final ModuleResolverFactory mrf = new DefaultModuleResolverFactory("platform:/plugin/" + EmftvmCompilerPlugin.PLUGIN_ID
 				+ "/transformations/");
 
@@ -112,8 +113,7 @@ public class AtlToEmftvmCompiler implements AtlStandaloneCompiler {
 	 * {@inheritDoc}
 	 */
 	@SuppressWarnings("deprecation")
-	public EObject[] compileWithProblemModel(final InputStream in,
-			final String outputFileName) {
+	public EObject[] compileWithProblemModel(final InputStream in, final String outputFileName) {
 		EObject[] result = new EObject[0];
 		try {
 			File asm = new File(outputFileName);
@@ -121,11 +121,16 @@ public class AtlToEmftvmCompiler implements AtlStandaloneCompiler {
 				asm.delete();
 			}
 			asm.createNewFile();
-			//TODO Refactor ATL's compiler framework to support multiple file extensions
+			// TODO Refactor ATL's compiler framework to support multiple file extensions
 			final String emftvmOutputFileName = outputFileName.substring(0, outputFileName.lastIndexOf('.')) + ".emftvm";
-			result = compileWithProblemModel(in, new FileOutputStream(emftvmOutputFileName));
-			final IFile[] outputFiles = ResourcesPlugin.getWorkspace().getRoot().findFilesForLocationURI(
-					java.net.URI.create("file:/" + emftvmOutputFileName.replace(' ', '+')));
+			final OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(emftvmOutputFileName));
+			try {
+				result = compileWithProblemModel(in, outputStream);
+			} finally {
+				outputStream.close();
+			}
+			final IFile[] outputFiles = ResourcesPlugin.getWorkspace().getRoot()
+					.findFilesForLocationURI(java.net.URI.create("file:/" + emftvmOutputFileName.replace(' ', '+')));
 			for (IFile file : outputFiles) {
 				file.getParent().refreshLocal(IResource.DEPTH_ONE, null);
 				if (file.exists()) {
@@ -145,14 +150,13 @@ public class AtlToEmftvmCompiler implements AtlStandaloneCompiler {
 	/**
 	 * {@inheritDoc}
 	 */
-	public EObject[] compileWithProblemModel(final InputStream in,
-			final OutputStream outputStream) {
+	public EObject[] compileWithProblemModel(final InputStream in, final OutputStream outputStream) {
 		final List<EObject> pbs = new ArrayList<EObject>();
 		try {
 			final IModel[] parsed = AtlParser.getDefault().parseToModelWithProblems(in, true);
 			final IModel atlmodel = parsed[0];
 			final IModel problems = parsed[1];
-			
+
 			if (getProblems(problems, pbs) == 0) {
 				final EObject[] cpbs = compileWithProblemModel(atlmodel, outputStream);
 				for (EObject cpb : cpbs) {
@@ -169,25 +173,24 @@ public class AtlToEmftvmCompiler implements AtlStandaloneCompiler {
 	/**
 	 * {@inheritDoc}
 	 */
-	public EObject[] compileWithProblemModel(IModel atlModel,
-			OutputStream outputStream) {
+	public EObject[] compileWithProblemModel(IModel atlModel, OutputStream outputStream) {
 		final List<EObject> pbs = new ArrayList<EObject>();
-		
+
 		final Model atlm = EmftvmFactory.eINSTANCE.createModel();
-		atlm.setResource(((EMFModel)atlModel).getResource());
+		atlm.setResource(((EMFModel) atlModel).getResource());
 
 		final Resource pr = rs.createResource(URI.createFileURI("problems.xmi"));
 		final Model pbm = EmftvmFactory.eINSTANCE.createModel();
 		pbm.setResource(pr);
-		
+
 		final Resource r = rs.createResource(URI.createFileURI("out.emftvm"), "org.eclipse.m2m.atl.emftvm");
 		final Model emftvmm = EmftvmFactory.eINSTANCE.createModel();
 		emftvmm.setResource(r);
-		
+
 		final Resource ri = rs.createResource(URI.createFileURI("inlined.emftvm"), "org.eclipse.m2m.atl.emftvm");
 		final Model emftvmmi = EmftvmFactory.eINSTANCE.createModel();
 		emftvmmi.setResource(ri);
-		
+
 		final ExecEnv atlWfrEnv = atlWfrPool.getExecEnv();
 		final ExecEnv atlToEmftvmEnv = atlToEmftvmPool.getExecEnv();
 		final ExecEnv inlineCodeblocksEnv = inlineCodeblocksPool.getExecEnv();
@@ -195,18 +198,18 @@ public class AtlToEmftvmCompiler implements AtlStandaloneCompiler {
 			atlWfrEnv.registerInputModel("IN", atlm);
 			atlWfrEnv.registerOutputModel("OUT", pbm);
 			atlWfrEnv.run(null);
-			
+
 			if (getProblems(pbm, pbs) == 0) {
 				atlToEmftvmEnv.registerInputModel("IN", atlm);
 				atlToEmftvmEnv.registerOutputModel("OUT", emftvmm);
 				atlToEmftvmEnv.registerOutputModel("PBS", pbm);
 				atlToEmftvmEnv.run(null);
-				
+
 				if (getProblems(pbm, pbs) == 0) {
 					inlineCodeblocksEnv.registerInputModel("IN", emftvmm);
 					inlineCodeblocksEnv.registerOutputModel("OUT", emftvmmi);
 					inlineCodeblocksEnv.run(null);
-					
+
 					ri.save(outputStream, Collections.emptyMap());
 				}
 			}
@@ -231,8 +234,7 @@ public class AtlToEmftvmCompiler implements AtlStandaloneCompiler {
 	/**
 	 * {@inheritDoc}
 	 */
-	public EObject[] compileWithProblemModel(final IModel atlModel,
-			final String outputFileName) {
+	public EObject[] compileWithProblemModel(final IModel atlModel, final String outputFileName) {
 		try {
 			File asm = new File(outputFileName);
 			if (asm.exists()) {
@@ -240,7 +242,12 @@ public class AtlToEmftvmCompiler implements AtlStandaloneCompiler {
 			}
 			asm.createNewFile();
 			final String emftvmOutputFileName = outputFileName.substring(0, outputFileName.lastIndexOf('.')) + ".emftvm";
-			return compileWithProblemModel(atlModel, new FileOutputStream(emftvmOutputFileName));
+			final OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(emftvmOutputFileName));
+			try {
+				return compileWithProblemModel(atlModel, outputStream);
+			} finally {
+				outputStream.close();
+			}
 		} catch (IOException e) {
 			ATLLogger.log(Level.SEVERE, e.getLocalizedMessage(), e);
 			EmftvmCompilerPlugin.log(e);
@@ -250,20 +257,23 @@ public class AtlToEmftvmCompiler implements AtlStandaloneCompiler {
 
 	/**
 	 * Retrieves problem elements from <code>problems</code>.
-	 * @param problems the problems model
-	 * @param pbElements the collection of problem elements to augment
+	 * 
+	 * @param problems
+	 *            the problems model
+	 * @param pbElements
+	 *            the collection of problem elements to augment
 	 * @return the number of error problems
 	 */
 	@SuppressWarnings("unchecked")
 	protected int getProblems(IModel problems, Collection<EObject> pbElements) {
-		final Collection<EObject> pbs = (Collection<EObject>)problems.getElementsByType(
-				problems.getReferenceModel().getMetaElementByName("Problem")); //$NON-NLS-1$
+		final Collection<EObject> pbs = (Collection<EObject>) problems.getElementsByType(problems.getReferenceModel().getMetaElementByName(
+				"Problem")); //$NON-NLS-1$
 
 		int nbErrors = 0;
 		if (pbs != null) {
 			for (EObject pb : pbs) {
 				EStructuralFeature severityFeature = pb.eClass().getEStructuralFeature("severity"); //$NON-NLS-1$
-				if (severityFeature != null && "error".equals(((EEnumLiteral)pb.eGet(severityFeature)).getName())) { //$NON-NLS-1$
+				if (severityFeature != null && "error".equals(((EEnumLiteral) pb.eGet(severityFeature)).getName())) { //$NON-NLS-1$
 					nbErrors++;
 				}
 			}
@@ -275,19 +285,21 @@ public class AtlToEmftvmCompiler implements AtlStandaloneCompiler {
 
 	/**
 	 * Retrieves problem elements from <code>problems</code>.
-	 * @param problems the problems model
-	 * @param pbElements the collection of problem elements to augment
+	 * 
+	 * @param problems
+	 *            the problems model
+	 * @param pbElements
+	 *            the collection of problem elements to augment
 	 * @return the number of error problems
 	 */
 	protected int getProblems(Model problems, Collection<EObject> pbElements) {
-		final Collection<EObject> pbs = (Collection<EObject>)problems.allInstancesOf(
-				(EClass)pbmm.findType("Problem")); //$NON-NLS-1$
+		final Collection<EObject> pbs = (Collection<EObject>) problems.allInstancesOf((EClass) pbmm.findType("Problem")); //$NON-NLS-1$
 
 		int nbErrors = 0;
 		if (pbs != null) {
 			for (EObject pb : pbs) {
 				EStructuralFeature severityFeature = pb.eClass().getEStructuralFeature("severity"); //$NON-NLS-1$
-				if (severityFeature != null && "error".equals(((EEnumLiteral)pb.eGet(severityFeature)).getName())) { //$NON-NLS-1$
+				if (severityFeature != null && "error".equals(((EEnumLiteral) pb.eGet(severityFeature)).getName())) { //$NON-NLS-1$
 					nbErrors++;
 				}
 			}
