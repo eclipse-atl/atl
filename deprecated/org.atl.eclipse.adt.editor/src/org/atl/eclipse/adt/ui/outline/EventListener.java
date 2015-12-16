2ee6d4bc4b7652ecf247094f1c13a319c28721da
William Piers <william.piers@obeo.fr>
2015-12-16 14:46:12 +0100

    removed old unused plugins

--- deprecated/org.atl.eclipse.adt.editor/src/org/atl/eclipse/adt/ui/outline/EventListener.java
+++
@@ -1,6 +0,0 @@
-package org.atl.eclipse.adt.ui.outline;
-
-public interface EventListener 
-{
-	public void update(Event evt);
-}
--- deprecated/org.atl.eclipse.engine/add-jars-to-maven.sh
+++
@@ -1,6 +0,0 @@
-## !/bin/sh
-
-mvn deploy:deploy-file -DrepositoryId=andromda-repository -Durl=scp://galaxy.andromda.org/var/www/andromda/maven2 -Dfile=lib/ebnfinjector.jar  -DgroupId=org.atl.eclipse.engine -DartifactId=ebnfinjector  -Dversion=1.13 -Dpackaging=jar -DgeneratePom=true
-mvn deploy:deploy-file -DrepositoryId=andromda-repository -Durl=scp://galaxy.andromda.org/var/www/andromda/maven2 -Dfile=lib/ebnfextractor.jar -DgroupId=org.atl.eclipse.engine -DartifactId=ebnfextractor -Dversion=1.14 -Dpackaging=jar -DgeneratePom=true
-mvn deploy:deploy-file -DrepositoryId=andromda-repository -Durl=scp://galaxy.andromda.org/var/www/andromda/maven2 -Dfile=lib/ATL-importer.jar  -DgroupId=org.atl.eclipse.engine -DartifactId=atl-importer  -Dversion=1.9  -Dpackaging=jar -DgeneratePom=true
-
