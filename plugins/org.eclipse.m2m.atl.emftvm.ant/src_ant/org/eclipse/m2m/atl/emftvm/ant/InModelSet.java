10ce5772f4b6f39ea0725b80041dbba1b7f9b1b4
Dennis Wagelaar <dwagelaar@gmail.com>
2021-02-04 20:43:22 +0100

    570932: [Feature] Support batch transformation in Ant
    Change-Id: Ide818169ed2b9812b9ae1e4fbcf390522a74fdbc
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=570932
    Signed-off-by: Dennis Wagelaar <dwagelaar@gmail.com>

---
+++ plugins/org.eclipse.m2m.atl.emftvm.ant/src_ant/org/eclipse/m2m/atl/emftvm/ant/InModelSet.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Dennis Wagelaar.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * Contributors:
+ *     Dennis Wagelaar - initial API and
+ *         implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.m2m.atl.emftvm.ant;
+
+/**
+ * Nested Ant input model element set.
+ * 
+ * @author <a href="mailto:dwagelaar@gmail.com">Dennis Wagelaar</a>
+ */
+public class InModelSet extends ModelElementSet {
+
+}
