79eed684ad032083cc7ea2802a3a1d8bfc1f8715
Dennis Wagelaar <dwagelaar@gmail.com>
2021-02-05 18:57:49 +0100

    570932: [Feature] Support batch transformation in Ant
    Change-Id: I2fed99e8bcf60202d194f03259bca449a32a21dd
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=570932

--- plugins/org.eclipse.m2m.atl.emftvm.ant/src_ant/org/eclipse/m2m/atl/emftvm/ant/CompileTask.java
+++ plugins/org.eclipse.m2m.atl.emftvm.ant/src_ant/org/eclipse/m2m/atl/emftvm/ant/CompileTask.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Dennis Wagelaar.
+ * Copyright (c) 2018, 2021 Dennis Wagelaar.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -13,11 +13,17 @@ package org.eclipse.m2m.atl.emftvm.ant;
 
 import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
+import org.apache.tools.ant.types.FileSet;
+import org.apache.tools.ant.types.Resource;
+import org.apache.tools.ant.types.resources.FileResource;
+import org.apache.tools.ant.util.StringUtils;
 import org.eclipse.m2m.atl.common.ATLLogger;
 import org.eclipse.m2m.atl.core.ATLCoreException;
 import org.eclipse.m2m.atl.emftvm.compiler.AtlToEmftvmCompiler;
@@ -33,17 +39,19 @@ public class CompileTask extends EMFTVMTask {
 	private static final String DOT_ATL = ".atl";
 	private static final String DOT_EMFTVM = ".emftvm";
 	private static final double DIVISOR = 1.0E9;
+	private static final Pattern DOT_ATL_PAT = Pattern.compile("\\.atl$");
 
 	private String module;
 	private String modulePath;
 	private String charset;
 	private String outputPath;
+	private final List<FileSet> moduleSets = new ArrayList<FileSet>();
+	private AtlToEmftvmCompiler compiler;
 
 	/**
 	 * Sets the module name.
 	 *
-	 * @param module
-	 *            the module to set
+	 * @param module the module to set
 	 */
 	public void setModule(String module) {
 		this.module = module;
@@ -62,8 +70,7 @@ public class CompileTask extends EMFTVMTask {
 	 * Sets the module path. If not set,
 	 * {@link org.apache.tools.ant.Project#getBaseDir()} is used.
 	 *
-	 * @param modulePath
-	 *            the modulePath to set
+	 * @param modulePath the modulePath to set
 	 */
 	public void setModulePath(String modulePath) {
 		this.modulePath = modulePath;
@@ -91,8 +98,7 @@ public class CompileTask extends EMFTVMTask {
 	/**
 	 * Sets the character set to be used for parsing.
 	 *
-	 * @param charset
-	 *            the character set to set
+	 * @param charset the character set to set
 	 */
 	public void setCharset(String charset) {
 		this.charset = charset;
@@ -112,11 +118,40 @@ public class CompileTask extends EMFTVMTask {
 	 * Sets the directory path to write the output file to. If not set,
 	 * {@link #getModulePath()} is used.
 	 *
-	 * @param outputPath
-	 *            the outputPath to set
+	 * @param outputPath the outputPath to set
 	 */
 	public void setOutputPath(String outputPath) {
 		this.outputPath = outputPath;
+	}
+
+	/**
+	 * Returns the module {@link FileSet}.
+	 *
+	 * @return the moduleSets
+	 */
+	public List<FileSet> getModuleSets() {
+		return moduleSets;
+	}
+
+	/**
+	 * Adds module set to the compile task.
+	 *
+	 * @param moduleSet the module set to add
+	 */
+	public void addConfiguredModuleSet(FileSet moduleSet) {
+		getModuleSets().add(moduleSet);
+	}
+
+	/**
+	 * Returns the {@link AtlToEmftvmCompiler}.
+	 *
+	 * @return the compiler
+	 */
+	protected AtlToEmftvmCompiler getCompiler() {
+		if (compiler == null) {
+			compiler = new AtlToEmftvmCompiler();
+		}
+		return compiler;
 	}
 
 	/**
@@ -124,22 +159,39 @@ public class CompileTask extends EMFTVMTask {
 	 */
 	@Override
 	protected void innerExecute() throws Exception {
+		final String module = getModule();
+		if (StringUtils.trimToNull(module) != null) {
+			final File modulePath = trimToBaseDir(getModulePath());
+			final Resource resource = new FileResource(modulePath, module + DOT_ATL);
+			compileResource(modulePath, resource);
+		}
+
+		for (final FileSet moduleSet : getModuleSets()) {
+			final File dir = moduleSet.getDir();
+			final File modulePath = dir == null ? getProject().getBaseDir() : dir;
+			for (final Resource resource : moduleSet) {
+				compileResource(modulePath, resource);
+			}
+		}
+	}
+
+	private void compileResource(final File basedir, final Resource resource) throws Exception {
 		final long startTimeNanos = System.nanoTime();
 
-		final String modulePath = trimToBaseDir(getModulePath());
-		final String module = getModule();
-		final String inputFilePath = modulePath + module + DOT_ATL;
-		final AtlToEmftvmCompiler compiler = new AtlToEmftvmCompiler();
-		final InputStream inputStream = new FileInputStream(inputFilePath);
+		final String modulePath = basedir + File.separator;
+		final String module = resource.getName();
+		final String inputFilePath = modulePath + module;
+		final InputStream inputStream = resource.getInputStream();
 		try {
 			final BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
 			final String charset = getCharset();
 			final Reader reader = charset == null ? new InputStreamReader(bufferedInputStream)
 					: new InputStreamReader(bufferedInputStream, charset);
 			final String outputPath = getOutputPath();
-			final String outputFilePath = (outputPath != null ? outputPath : modulePath) + module + DOT_EMFTVM;
+			final String outputFilePath = (outputPath != null ? outputPath : modulePath)
+					+ DOT_ATL_PAT.matcher(module).replaceAll(DOT_EMFTVM);
 
-			final CompileTimeError[] errors = compiler.compile(reader, outputFilePath);
+			final CompileTimeError[] errors = getCompiler().compile(reader, outputFilePath);
 
 			if (errors != null && errors.length > 0) {
 				final StringBuilder errorString = new StringBuilder();
@@ -160,14 +212,13 @@ public class CompileTask extends EMFTVMTask {
 	/**
 	 * Trims <code>null</code> strings to the project's BaseDir.
 	 *
-	 * @param str
-	 *            the string to trim
+	 * @param str the string to trim
 	 * @return <code>str</code>, or
 	 *         {@link org.apache.tools.ant.Project#getBaseDir()} if
 	 *         <code>null</code>
 	 */
-	private String trimToBaseDir(final String str) {
-		return str != null ? str : getProject().getBaseDir().getPath() + File.separator;
+	private File trimToBaseDir(final String str) {
+		return str != null ? new File(str) : getProject().getBaseDir();
 	}
 
 }
